8c2e504acf3b3888f677d4b9744674bdbb425d8a
Hariom <hariomsahuu2005@gmail.com>
2026-04-09 15:28:54 +0530

    deleted old one files

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,17 @@
+import BambooProcess from "@/components/(homePage)/bambooProcess";
 import HeroSection from "@/components/(homePage)/heroSection";
 import ProductsSection from "@/components/(homePage)/productsSection";
+import Testimonials from "@/components/(homePage)/testimonial";
+import WhyUs from "@/components/(homePage)/why";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <ProductsSection />
+      <BambooProcess />
+      <WhyUs />
+      <Testimonials />
     </>
   );
 }
