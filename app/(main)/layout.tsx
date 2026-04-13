748ac758b86b5ee423a5da30fae5980848e4e816
Hariom <hariomsahuu2005@gmail.com>
2026-04-13 14:45:13 +0530

    added vercel speed-insight

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/header";
 import { CartProvider } from "@/context/cartContext";
 import CartDrawer from "@/components/cartDrawer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Bambumm",
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata: Metadata = {
   title: "Bambumm",
   description: "Undergarments for men and women",
