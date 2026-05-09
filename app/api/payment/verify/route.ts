0b3e9e0ac6551221cb96cd104df2f3d817493874
Hariom <hariomsahuu2005@gmail.com>
2026-05-09 17:30:51 +0530

    fixed verify route api

--- app/api/payment/verify/route.ts
+++ app/api/payment/verify/route.ts
@@ -41,7 +41,7 @@ export async function POST(req: NextRequest) {
 
     const body = razorpay_order_id + "|" + razorpay_payment_id;
     const expectedSignature = crypto
-      .createHmac("sha256", process.env.RAZORPAY__KEY_SECRET!)
+      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET!)
       .update(body)
       .digest("hex");
 
